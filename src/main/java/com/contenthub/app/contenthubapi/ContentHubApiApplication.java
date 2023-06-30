cc5870c7c12e8d464f3909162dc6a645b5193113
Prakhar <38278156+MishraPrakhar@users.noreply.github.com>
2023-06-30 17:01:24 +0100

    Initial commit

---
+++ src/main/java/com/contenthub/app/contenthubapi/ContentHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.contenthub.app.contenthubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentHubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contenthub/app/contenthubapi/ContentHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contenthub.app.contenthubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
